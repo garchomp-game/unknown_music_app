3e1f72c426b0443488d73277f9dc9b5bed0f2349
SyoInoue <funnyfacee7777@gmail.com>
2022-09-28 00:14:07 +0900

    LoginPage_Create

--- front/app/components/Header.tsx
+++ front/app/components/Header.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [thisOpen, setThisOpen] = useState(false);
 
-//スマホ用ハンバーガーメニュー
   const dropdownRef = useRef();
   useEffect(() => {
     document.addEventListener("mousedown", handleOutSideClick);
@@ -13,21 +13,50 @@ const Header = () => {
       document.removeEventListener("mousedown", handleOutSideClick);
     };
   }, []);
-
-  //右上の人アイコン
   const handleOutSideClick = (e) => {
     if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
       setIsOpen(false);
+      setThisOpen(false);
     }
   };
 
   return (
       <nav className="left-0 block fixed w-full bg-gray-900 z-50 bg-opacity-95 shadow-2xl">
+        {/* スマホ用ハンバーガーメニュー */}
         <div ref={dropdownRef}>
           <div className="max-w-7xl mx-auto xl:mx-44 md:mx-20">
             <div className="relative flex items-center justify-between h-16">
+                <div className="ml-6 flex items-center flex-row flex-col sm:hidden">
+                    <button
+                      type="button"
+                      className="bg-gray-900 bg-opacity-0 shadow-2xl rounded-md p-2 inline-flex items-center justify-center text-white hover:opacity-70 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                      aria-expanded="true"
+                      onClick={() => setThisOpen(!thisOpen)}
+                      >
+                      <span className="sr-only">Open main menu</span>
+                      <svg
+                        className="w-6 h-6"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M4 6h16M4 12h16M4 18h16"
+                        ></path>
+                      </svg>
+                    </button>
+                </div>
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex-shrink-0 flex items-center">
+                <div className="flex-shrink-0 flex items-center hover:opacity-70 sm:mr-0 mr-16">
+                  <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+                    <div className="flex items-center justify-between w-full md:w-auto">
+                      <span className="sr-only">Workflow</span>
+                    </div>
+                  </div>
                   <Link href="/">
                     <img
                       className="sm:ml-10 block h-7 w-auto"
@@ -69,12 +98,13 @@ const Header = () => {
                   </div>
                 </div>
               </div>
+              {/* ユーザーメニュー*/}
               <div className="absolute inset-y-0 right-0 flex items-center sm:static sm:inset-auto sm:ml-6 sm:pr-1">
                 <div className="ml-3 relative z-30 md:mr-0 mr-10">
                   <div>
                     <button
                       type="button"
-                      className="flex text-sm rounded-full text-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                      className="flex text-sm rounded-full text-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-8 focus:ring-offset-gray-900 focus:ring-indigo-500 hover:opacity-70"
                       id="user-menu"
                       aria-expanded="false"
                       aria-haspopup="true"
@@ -143,6 +173,73 @@ const Header = () => {
             </div>
           </div>
         </div>
+        {/* スマホ用ハンバーガーメニュー */}
+        <Transition
+            show={thisOpen}
+            enter="duration-150 ease-out "
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="duration-100 ease-in "
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            {(ref) => (
+              <div
+                ref={ref}
+                className="absolute -top-1 inset-x-0 p-2 max-w-md transition transform origin-top-right sm:hidden">
+                <div className="rounded-lg shadow-md w-5/12 bg-gray-900 ring-1 ring-black ring-opacity-5 overflow-hidden pb-0 bg-opacity-95">
+                  <div className="px-2 pt-2 pb-3 space-y-1 sm:hidden block">
+                  <a
+                      href="#"
+                      className="block px-3 py-2 rounded-md text-base font-medium text-gray-200 hover:text-gray-200 hover:bg-gray-400"
+                    >
+                      アプリの使い方
+                    </a>
+                    <a
+                      href="/admin-page"
+                      className="block px-3 py-2 rounded-md text-base font-medium text-gray-200 hover:text-gray-200 hover:bg-gray-400"
+                    >
+                      運営者情報
+                    </a>
+
+                    <a
+                      href="#"
+                      className="block px-3 py-2 rounded-md text-base font-medium text-gray-200 hover:text-gray-200 hover:bg-gray-400"
+                    >
+                      利用規約
+                    </a>
+                    <a
+                      href="/contact-form"
+                      className="block px-3 py-2 rounded-md text-base font-medium text-gray-200 hover:text-gray-200 hover:bg-gray-400"
+                    >
+                      お問い合わせ
+                    </a>
+                  </div>
+                    <div className="px-2 pt-2 pb-3 space-y-1 hidden sm:block">
+                      <a
+                        href="/admin-page"
+                        className="block px-3 py-2 rounded-md text-base font-medium text-gray-200 hover:text-gray-200 hover:bg-gray-400"
+                      >
+                        Unknown_Musicとは
+                      </a>
+
+                      <a
+                        href="#"
+                        className="block px-3 py-2 rounded-md text-base font-medium text-gray-200 hover:text-gray-200 hover:bg-gray-400"
+                      >
+                        コンテンツ
+                      </a>
+                      <a
+                        href="/contact-form"
+                        className="block px-3 py-2 rounded-md text-base font-medium text-gray-200 hover:text-gray-200 hover:bg-gray-400"
+                      >
+                        使ってみる
+                      </a>
+                    </div>
+                  </div>
+                </div>
+            )}
+          </Transition>
       </nav>
   );
 }
